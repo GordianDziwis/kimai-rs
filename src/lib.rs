a34103513da9e1e6a784049274494231be9bcbdf
Björn Guth <b.guth@netcon-consulting.com>
2021-06-30 18:20:03 +0200

    Create a subcommand to list all customers

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use serde::Deserialize;
+use clap::crate_name;
+use prettytable::{cell, format, row, Table};
+use reqwest::header::{self, HeaderName, HeaderValue};
+use serde::{Deserialize, Serialize};
 use std::fmt;
 use std::fs;
 use std::path::Path;
@@ -10,6 +13,7 @@ pub enum KimaiError {
     IO(String),
     Toml(String),
     Utf8(String),
+    Reqwest(String),
     Config(String),
     Other(String),
 }
@@ -23,6 +27,7 @@ impl fmt::Display for KimaiError {
             KimaiError::IO(e) => write!(f, "IO Error: {}", e),
             KimaiError::Toml(e) => write!(f, "TOML Error: {}", e),
             KimaiError::Utf8(e) => write!(f, "UTF-8 Error: {}", e),
+            KimaiError::Reqwest(e) => write!(f, "Reqwest Error: {}", e),
             KimaiError::Config(e) => write!(f, "Config Error: {}", e),
             KimaiError::Other(e) => write!(f, "Error: {}", e),
         }
@@ -50,6 +55,12 @@ impl From<toml::de::Error> for KimaiError {
 impl From<std::str::Utf8Error> for KimaiError {
     fn from(error: std::str::Utf8Error) -> KimaiError {
         KimaiError::Utf8(error.to_string())
+    }
+}
+
+impl From<reqwest::Error> for KimaiError {
+    fn from(error: reqwest::Error) -> KimaiError {
+        KimaiError::Reqwest(error.to_string())
     }
 }
 
@@ -100,7 +111,7 @@ impl Config {
     }
 
     pub fn from_xdg() -> Result<Self, KimaiError> {
-        let xdg_dirs = xdg::BaseDirectories::with_prefix("kimai")?;
+        let xdg_dirs = xdg::BaseDirectories::with_prefix(crate_name!())?;
         let config_path = xdg_dirs
             .find_config_file("config.toml")
             .ok_or_else(|| KimaiError::Config("config file not found!".to_string()))?;
@@ -108,6 +119,60 @@ impl Config {
     }
 }
 
-pub fn main() {
-    println!("{:#?}", Config::from_xdg());
+fn get_headers(config: &Config) -> Result<header::HeaderMap, KimaiError> {
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        HeaderName::from_static("x-auth-user"),
+        HeaderValue::from_str(&config.user).unwrap(),
+    );
+    headers.insert(
+        HeaderName::from_static("x-auth-token"),
+        HeaderValue::from_str(&config.password).unwrap(),
+    );
+    Ok(headers)
+}
+
+fn make_get_request(
+    config: &Config,
+    api_endpoint: &str,
+) -> Result<reqwest::blocking::Response, KimaiError> {
+    let url = format!("{}/{}", config.host, api_endpoint);
+    let client = reqwest::blocking::Client::builder()
+        .default_headers(get_headers(config)?)
+        .build()
+        .unwrap();
+    Ok(client.get(&url).send()?)
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Customer {
+    id: usize,
+    name: String,
+    visible: bool,
+    color: String,
+}
+
+pub fn get_customers(config_path: Option<String>) -> Result<Vec<Customer>, KimaiError> {
+    let config = match config_path {
+        Some(p) => Config::from_path(Path::new(&p)),
+        None => Config::from_xdg(),
+    }?;
+
+    let response = make_get_request(&config, "api/customers")?;
+    Ok(response.json::<Vec<Customer>>()?)
+}
+
+pub fn print_customers(config_path: Option<String>) -> Result<(), KimaiError> {
+    let customers = get_customers(config_path)?;
+
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+    table.set_titles(row!["ID", "Name"]);
+    for customer in customers {
+        table.add_row(row![customer.id, customer.name]);
+    }
+
+    table.printstd();
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, App, Arg, SubCommand};
+
 fn main() {
-    kimai::main();
+    let config_path_arg = Arg::with_name("config_path")
+        .short("c")
+        .long("config_path")
+        .help("Path to a config file")
+        .takes_value(true);
+
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .subcommand(
+            SubCommand::with_name("customers")
+                .author(crate_authors!())
+                .version(crate_version!())
+                .about("Get a list of all customers")
+                .arg(&config_path_arg),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("customers") {
+        kimai::print_customers(matches.value_of("config_path").map(|p| p.to_string())).unwrap();
+    }
 }
